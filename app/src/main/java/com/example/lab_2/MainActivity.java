74393f81bd32476465e9829218f19b28d19872db
SVitaliiR <vitaliyruslanov@gmail.com>
2023-10-10 09:21:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab_2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.lab_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner ItemsToCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
