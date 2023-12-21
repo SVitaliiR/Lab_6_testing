cf8df19a671eff8581827b36b451c811ac44d6cb
SVitaliiR <vitaliyruslanov@gmail.com>
2023-12-21 18:42:17 +0200

    Added UI tests for wordcounter lab

--- app/src/androidTest/java/com/example/lab_2/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/lab_2/ExampleInstrumentedTest.java
@@ -5,10 +5,26 @@ import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.AllOf.allOf;
 import static org.junit.Assert.*;
+
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
+
+
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +32,58 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.lab_2", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testCharCount() {
+        // Input text
+        Espresso.onView(withId(R.id.textEditorUI))
+                .perform(ViewActions.typeText("Hello World"));
+
+        Espresso.onView(withId(R.id.itemsToCount))
+                .perform(ViewActions.click());
+        Espresso.onData(allOf(is(instanceOf(String.class)), is("Chars"))).perform(ViewActions.click());
+
+        // Click on count button
+        Espresso.onView(withId(R.id.countWordButton))
+                .perform(ViewActions.click());
+
+        Espresso.onView(withId(R.id.textEditorUI))
+                        .check(matches(withText("Hello World")));
+
+        // Check result
+        Espresso.onView(withId(R.id.tVResult))
+                .check(matches(withText("11")));
+    }
+
+    @Test
+    public void testWordCount() {
+        // Input text
+        Espresso.onView(withId(R.id.textEditorUI))
+                .perform(ViewActions.typeText("Hello World"));
+
+        // Select "Words" from spinner
+        Espresso.onView(withId(R.id.itemsToCount))
+                .perform(ViewActions.click());
+        Espresso.onData(allOf(is(instanceOf(String.class)), is("Words"))).perform(ViewActions.click());
+
+        // Click on count button
+        Espresso.onView(withId(R.id.countWordButton))
+                .perform(ViewActions.click());
+
+        // Check result
+        Espresso.onView(withId(R.id.tVResult))
+                .check(matches(withText("2")));
     }
 }
--- app/src/test/java/com/example/lab_2/ExampleUnitTest.java
+++ app/src/test/java/com/example/lab_2/ExampleUnitTest.java
@@ -19,42 +19,42 @@ public class ExampleUnitTest {
     public void testWordCount() {
         String text = "Hello World";
         int result = TextCounter.getWordCount(text);
-        assertEquals(2, result);  // Assuming "Hello World" has 2 words
+        assertEquals(2, result);
     }
 
     @Test
     public void testCharCount() {
         String text = "Hello";
         int result = CharsCounter.getChartsCount(text);
-        assertEquals(5, result);  // Assuming "Hello" has 5 characters
+        assertEquals(5, result);
     }
 
     @Test
     public void testWordCount_EmptyText() {
         String text = "";
         int result = TextCounter.getWordCount(text);
-        assertEquals(0, result);  // An empty string should have 0 words
+        assertEquals(0, result);
     }
 
     @Test
     public void testWordCount_OnlyWhitespace() {
         String text = "  \t  ";
         int result = TextCounter.getWordCount(text);
-        assertEquals(0, result);  // Only whitespace should be considered 0 words
+        assertEquals(0, result);
     }
 
     @Test
     public void testWordCount_MultipleSpaces() {
         String text = "Hello   World";
         int result = TextCounter.getWordCount(text);
-        assertEquals(2, result);  // Multiple spaces should be treated as one word separator
+        assertEquals(2, result);
     }
 
     @Test
     public void testWordCount_SpecialCharacters() {
         String text = "Hello! World?";
         int result = TextCounter.getWordCount(text);
-        assertEquals(2, result);  // Special characters should not split words
+        assertEquals(2, result);
     }
 
 
@@ -62,28 +62,28 @@ public class ExampleUnitTest {
     public void testCharCount_EmptyText() {
         String text = "";
         int result = CharsCounter.getChartsCount(text);
-        assertEquals(0, result);  // An empty string should have 0 characters
+        assertEquals(0, result);
     }
 
     @Test
     public void testCharCount_WithWhitespace() {
         String text = "Hello World";
         int result = CharsCounter.getChartsCount(text);
-        assertEquals(11, result);  // Including spaces
+        assertEquals(11, result);
     }
 
     @Test
     public void testCharCount_SpecialCharacters() {
         String text = "Hello! World?";
         int result = CharsCounter.getChartsCount(text);
-        assertEquals(13, result);  // Including special characters
+        assertEquals(13, result);
     }
 
     @Test
     public void testCharCount_MultiByteCharacters() {
         String text = "你好";
         int result = CharsCounter.getChartsCount(text);
-        assertEquals(2, result);  // Counting multi-byte characters as two characters
+        assertEquals(2, result);
     }
 
 }
